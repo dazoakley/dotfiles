d422d4ce7ebb07081cdffaa49c6bddd07ae67bd5
Darren Oakley <darren.oakley@hmrc.gov.uk>
2025-01-09 14:54:22 +0000

    lazyvim updates

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,35 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local usercmd = vim.api.nvim_create_user_command
+
+--[[ -------------
+     AUTO COMMANDS
+     ------------- ]]
+
+-- Prefer creating groups and assigning autocmds to groups, because it makes it easier to clear them
+--[[ Mygroup Group ]]
+augroup("mygroup", { clear = true })
+
+-- ToggleTerm
+function _G.set_terminal_keymaps()
+  local opts = { noremap = true }
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+autocmd("TermOpen", {
+  pattern = "term://*toggleterm#*",
+  callback = function()
+    set_terminal_keymaps()
+  end,
+  group = "mygroup",
+  desc = "Set terminal mappings for ToggleTerm",
+})
